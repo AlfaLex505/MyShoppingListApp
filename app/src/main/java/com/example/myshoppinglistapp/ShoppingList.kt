f378c56faa7dfb6fc94f7eb9df90c460e65f47a0
Alexis Alfaro <alexisalfaro505@gmail.com>
2025-03-17 22:32:10 -0600

    Change the UI and add parameters for the classes.

--- app/src/main/java/com/example/myshoppinglistapp/ShoppingList.kt
+++ app/src/main/java/com/example/myshoppinglistapp/ShoppingList.kt
@@ -1,5 +1,6 @@
 package com.example.myshoppinglistapp
 
+import android.content.Context
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -17,6 +18,7 @@ import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -34,12 +36,24 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.locationapp.LocationUtils
 
-data class ShoppingItem(val id: Int, var name: String, var quantity: Int, var isEditing: Boolean = false)
+data class ShoppingItem(val id: Int,
+                        var name: String,
+                        var quantity: Int,
+                        var isEditing: Boolean = false,
+                        var address: String = "")
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ShoppingListApp() {
+fun ShoppingListApp(
+    locationUtils: LocationUtils,
+    viewModel: LocationViewModel,
+    navController: NavController,
+    context: Context,
+    address: String
+) {
     var sItems by remember { mutableStateOf(listOf<ShoppingItem>()) }
     var itemName by remember { mutableStateOf("") }
     var itemQuantity by remember { mutableStateOf("") }
@@ -163,8 +177,22 @@ fun ShoppingListItem(item: ShoppingItem,
 
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
-        Text(text = item.name, modifier = Modifier.padding(8.dp))
-        Text(text = "Qty: ${item.quantity}", modifier = Modifier.padding(8.dp))
+        Column(modifier = Modifier
+            .weight(1f)
+            .padding(8.dp)) {
+            Row {
+                Text(text = item.name, modifier = Modifier.padding(8.dp))
+                Text(text = "Qty: ${item.quantity}", modifier = Modifier.padding(8.dp))
+            }
+            Row(modifier = Modifier.fillMaxWidth()) {
+                Icon(imageVector = Icons.Default.LocationOn, contentDescription = null)
+                Text(text = item.address)
+            }
+        }
+
+
+
+
         Row(  modifier = Modifier
             .padding(8.dp)) {
             IconButton(
